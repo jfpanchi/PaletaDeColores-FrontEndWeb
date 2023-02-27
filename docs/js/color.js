4a53180e92fdd0f67901385284137e98769c6a6d
jfpanchi <jfpanchi2@espe.edu.ec>
2023-02-27 14:47:12 -0500

    Desarrollo almacenamiento
    Se desarrollo el almacenamiento persistente con LocalStorage

--- docs/js/color.js
+++ docs/js/color.js
@@ -2,9 +2,24 @@ const color = document.querySelector('#color');
 const codigo = document.querySelector('#codigo');
 const salida = document.querySelector('#salida')
 
+/* localStorage.setItem("colorValue","#ffffff"); */
+const almacenamiento = localStorage.getItem('colorValue') || "#ffffff";
+color.value = almacenamiento;
 
-color.addEventListener("input", ()=>{
+const setColor = () => {
     const valor = color.value;
     codigo.innerHTML = valor;
     salida.style.background = valor;
-})
+}
+
+setColor();
+
+const saveColor = (valor) => {
+    localStorage.setItem('colorValue',valor);
+}
+
+color.addEventListener("input",setColor);
+
+color.addEventListener("change", ()=>{
+    saveColor(color.value);
+});
